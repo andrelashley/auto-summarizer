c06c41ddb525d64b0790c0db72c070d426f7bd0a
Andre Lashley <andre.lashley@gmail.com>
2016-11-25 10:47:56 -0800

    Setup initial workflow

--- extension.js
+++ extension.js
@@ -1,30 +1,33 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+let summarize = (value) => {
+    console.log(value);
+};
+
 function activate(context) {
+    let summarizerDisposable = vscode.commands.registerCommand('extension.autoSummarize', () => {
+        let editor = vscode.window.activeTextEditor;
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "auto-summarizer" is now active!');
+        if(!editor) {
+            vscode.window.showErrorMessage("Cannot summarize text because no document is open.");
+            return;
+        }
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', function () {
-        // The code you place here will be executed every time your command is executed
+        let selection = editor.selection;
+        let text = editor.document.getText(selection);
 
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
+        if(!text){
+            vscode.window.showErrorMessage("No text selected. Please select some text and try again.");
+        }
+
+        summarize(text);
     });
 
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(summarizerDisposable);
+
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
 function deactivate() {
 }
 exports.deactivate = deactivate;

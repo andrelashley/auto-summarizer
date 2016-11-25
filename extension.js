4fb1970d0a7a9438fc8e10c60020dbf55a0a3cf0
Andre Lashley <andre.lashley@gmail.com>
2016-11-25 14:20:59 -0800

    - Wrap up summarization functionality - add unit tests

--- extension.js
+++ extension.js
@@ -1,30 +1,95 @@
 let vscode = require('vscode');
+let summarizer = require('node-summary');
 
-let summarize = (value) => {
-    console.log(value);
+const resultsTemplate = "${summaryResults.title}\n" +
+"${summaryResults.summary}\n" +
+"Original Length ${summaryResults.originalLength}\n" +
+"Summary Length ${summaryResults.summaryLength}\n" +
+"Summary Ratio ${summaryResults.summaryRatio}";
+
+let summarizeSelection = (originalTitle, originalText) => {
+    let originalLength = 0;
+    let summaryLength = 0;
+    let summaryRatio = 0;
+    let summaryResult = "";
+    originalText = originalText || "";
+    originalTitle = originalTitle || "";
+
+    summarizer.summarize(originalTitle, originalText, (err, summary) => {
+        if (err) {
+            vscode.window.showErrorMessage("Oops, something went wrong.");
+            return;
+        }
+
+        originalLength = originalTitle.length + originalText.length;
+        summaryLength = summary.length;
+        summaryRatio = (100 - (100 * (summary.length / (originalTitle.length + originalText.length))));
+        summaryResult = summary;
+    });
+
+    return {
+        title: originalTitle,
+        originalLength: originalLength,
+        summaryLength: summaryLength,
+        summaryRatio: summaryRatio,
+        summary: summaryResult
+    };
 };
+
+let insertText = (value) => {
+    let editor = vscode.window.activeTextEditor;
+
+    // unlikely, but let's check just in case
+    if (!editor) {
+        vscode.window.showErrorMessage("Cannot insert summary results because no document is open.");
+        return;
+    }
+
+    let selection = editor.selection;
+    let range = new vscode.Range(selection.start, selection.end);
+    editor.edit((editBuilder) => {
+        editBuilder.replace(range, value);
+    });
+};
+
+let fillResultsTemplate = (results) => {
+    let filledTemplate = resultsTemplate.replace('${summaryResults.title}', results.title);
+    filledTemplate = filledTemplate.replace('${summaryResults.summary}', results.summary);
+    filledTemplate = filledTemplate.replace('${summaryResults.originalLength}', results.originalLength);
+    filledTemplate = filledTemplate.replace('${summaryResults.summaryLength}', results.summaryLength);
+    filledTemplate = filledTemplate.replace('${summaryResults.summaryRatio}', results.summaryRatio);
+
+    return filledTemplate;
+};
+
+exports.services = { summarizeSelection: summarizeSelection, fillResultsTemplate: fillResultsTemplate };
 
 function activate(context) {
     let summarizerDisposable = vscode.commands.registerCommand('extension.autoSummarize', () => {
         let editor = vscode.window.activeTextEditor;
 
-        if(!editor) {
+        if (!editor) {
             vscode.window.showErrorMessage("Cannot summarize text because no document is open.");
             return;
         }
 
         let selection = editor.selection;
         let text = editor.document.getText(selection);
+        let title = "";
+        let summaryResults = {};
 
-        if(!text){
+        if (!text) {
             vscode.window.showErrorMessage("No text selected. Please select some text and try again.");
         }
 
-        summarize(text);
+        vscode.window.showInputBox({ prompt: "Title" })
+            .then(result => {
+                summaryResults = summarizeSelection(result, text);
+                insertText(fillResultsTemplate(summaryResults));
+            });
     });
 
     context.subscriptions.push(summarizerDisposable);
-
 }
 exports.activate = activate;
 
--- test/extension.test.js
+++ test/extension.test.js
@@ -1,24 +1,71 @@
+'use strict';
 /* global suite, test */
 
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
-// The module 'assert' provides assertion methods from node
 var assert = require('assert');
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 var vscode = require('vscode');
 var myExtension = require('../extension');
 
-// Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
 
-    // Defines a Mocha unit test
-    test("Something 1", function() {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+    test("Summarizing paragraph is successful", function() {
+        // arrange
+        let title = "Some text";
+        let content = "";
+        content +=" Although moreover mistaken kindness me feelings do be marianne. Son over own nay with tell they cold upon are. Cordial village and settled she ability law";
+        content += "herself. Finished why bringing but sir bachelor unpacked any thoughts. Unpleasing unsatiable particular inquietude did nor sir. Get his declared appetite";
+        content += "distance his together now families. Friends am himself at on norland it viewing. Suspected elsewhere you belonging continued commanded she.";
+        // act
+        var result = myExtension.services.summarizeSelection(title, content);
+        // assert
+        assert.notEqual("", result.summary);
     });
+
+    test("Summarizing paragraph fails with invalid content", function() {
+        // arrange
+        // break the summarization tool by passing in non-string values
+        let decimalTitle = 3.142;
+        let decimalContent = 3.142;
+        let exception = undefined;
+
+        // act
+        try {
+            var result = myExtension.services.summarizeSelection(decimalTitle, decimalContent);
+        } catch (error) {
+            exception = error;
+        }
+
+        // assert
+        assert.notEqual(undefined, exception);
+    });
+
+    test("Resulting template is not the same as the original template", function() {
+        // arrange
+        var unmodifiedTemplate = "${summaryResults.title}\n" +
+            "${summaryResults.summary}\n" +
+            "Original Length ${summaryResults.originalLength}\n" +
+            "Summary Length ${summaryResults.summaryLength}\n" +
+            "Summary Ratio ${summaryResults.summaryRatio}";
+
+        var templateCopy = unmodifiedTemplate;
+        let title = "Some text";
+        let content = "";
+        content +=" Although moreover mistaken kindness me feelings do be marianne. Son over own nay with tell they cold upon are. Cordial village and settled she ability law";
+        content += "herself. Finished why bringing but sir bachelor unpacked any thoughts. Unpleasing unsatiable particular inquietude did nor sir. Get his declared appetite";
+        content += "distance his together now families. Friends am himself at on norland it viewing. Suspected elsewhere you belonging continued commanded she.";
+
+        // act
+        var result = myExtension.services.summarizeSelection(title, content);
+
+        let filledTemplate = templateCopy.replace('${summaryResults.title}', result.title);
+        filledTemplate = filledTemplate.replace('${summaryResults.summary}', result.summary);
+        filledTemplate = filledTemplate.replace('${summaryResults.originalLength}', result.originalLength);
+        filledTemplate = filledTemplate.replace('${summaryResults.summaryLength}', result.summaryLength);
+        filledTemplate = filledTemplate.replace('${summaryResults.summaryRatio}', result.summaryRatio);
+
+        // assert
+        assert.notEqual(unmodifiedTemplate, filledTemplate);
+    });
+
+
 });
